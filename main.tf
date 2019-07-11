5ff33ab335e6d66ad245f60f32d6bd81e05c6e65
Les Aker <me@lesaker.org>
2019-07-11 19:50:37 +0000

    update module for tf 0.12

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ data "aws_iam_policy_document" "lambda_perms" {
 
 module "apigw" {
   source  = "armorfret/apigw-lambda/aws"
-  version = "0.0.3"
+  version = "0.1.0"
 
   source_bucket  = "${var.lambda_bucket}"
   source_version = "${var.lambda_version}"
