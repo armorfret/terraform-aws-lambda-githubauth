041306fc810a9fa04346dad06b05bb9c13754056
Les Aker <me@lesaker.org>
2023-08-01 21:21:34 -0400

    switch to trivy

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 module "publish-user" {
   source         = "armorfret/s3-publish/aws"
-  version        = "0.8.0"
+  version        = "0.8.1"
   logging_bucket = var.logging_bucket
   publish_bucket = var.config_bucket
 }
@@ -42,7 +42,7 @@ data "aws_iam_policy_document" "lambda_perms" {
 
 module "apigw" {
   source  = "armorfret/apigw-lambda/aws"
-  version = "0.7.1"
+  version = "0.7.3"
 
   source_bucket  = var.lambda_bucket
   source_version = var.lambda_version
