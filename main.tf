63758255d6d59ab33346247f13b33214894ab58c
Les Aker <me@lesaker.org>
2026-02-12 19:13:08 -0500

    update apigw

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ data "aws_iam_policy_document" "lambda_perms" {
 
 module "apigw" {
   source  = "armorfret/apigw-lambda/aws"
-  version = "0.10.0"
+  version = "0.11.0"
 
   source_bucket  = var.lambda_bucket
   source_version = var.lambda_version
